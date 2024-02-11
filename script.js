451c7df4f732fa823c7565952069f6667088a304
prokrishpatel <pro.krishpatel@gmail.com>
2024-02-11 16:35:07 +0530

    Final Release Of first Virson

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const apiKey="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";
+const url="https://api.openai.com/v1/chat/completions" ;
+
 //copy code
 const copyButton = document.getElementById("copyButton");
 copyButton.addEventListener("click", () => {
@@ -12,27 +15,95 @@ copyButton.addEventListener("click", () => {
 });
 
 //action
-const translate = document.getElementById("translate");
 const input = document.getElementById("codeInput");
 const inLangSelect = document.getElementById("languageSelect");
 const outLangSelect = document.getElementById("languageOut");
 const result = document.getElementById("output");
 
+const translateLanguage = document.getElementById("translateLanguage");
+const findBugs = document.getElementById("findBugs");
+const resolveBugs = document.getElementById("resolveBugs");
+const generateTestcases = document.getElementById("generateTestcases");
+const improveCode = document.getElementById("improveCode");
+
 //function to translate
-function trans(input,inL,outL){
-    let ans = "Your Desired Output";
-    //Translate input (inL->outL)
+async function getResult(code="",toDo="",changeTo="",changeFrom=""){
 
+    if(!code) return "NULL";
 
+    let message=[];
 
-    return ans;
+    switch (toDo) {
+        case "translateLanguage":
+            // Adding Prompts to Translate Language 
+            message.push({"role":"system", "content":`You are an excellent computer programming language code translator who can understand and translate syntax properly between varities of programing languages and leaves no error in resulted translated code by resolving errors in given code if any`});
+            message.push({"role":"user", "content":`Translate the given ${changeFrom} code  to ${changeTo} programming language with no bugs and tesed on rigorous test cases (Note: return only transtated code as an output not any other explanation or text) . Code: ${code}`});
+            break;
+
+        case "findBugs":
+            // Adding Prompts to Find Bugs 
+            message.push({"role":"system", "content":`You are excellent at understanding the given code and identifying and highlighting bugs in given code written in any prpgraming language if any`});
+            message.push({"role":"user", "content":`Identify the bugs in given ${changeFrom} code and highlight them if any. Code: ${code}`});
+            break;
+
+        case "resolveBugs":
+            message.push({"role":"system", "content":`You are excellent at understanding the given code and resloving bugs in given code written in any programming language if any and returns the bug free code.`});
+            message.push({"role":"user", "content":`Remove the bugs in given ${changeFrom} code if any (Note: return only bug free code as an output not any other explanation or text). Code: ${code}`});
+            break;
+
+        case "generateTestCase":
+            // Adding Prompts to Generate TestCase
+            message.push({"role":"system", "content":`You are excellent at understanding the given code written in any prpgraming language and generate rigorous testcases that the code need to pass in order to solve the problem that code trying to solve. generated cases may include TLE(time limit excced) cases , memory limit excced , the worse caes , the logical tast case that is not handeled in a code but may occur in solving problem that is tackeled by code , and many more`});
+            message.push({"role":"user", "content":`Generate rigorous testcases for given ${changeFrom} code (Note: return only generated testcaes not much of any explanation or any other text). Code: ${code}`});
+            break;
+
+        case "improveCode":
+            // Adding Prompts to Improve Code
+            message.push({"role":"system", "content":`You are excellent at understanding the given code written in any prpgraming language and improves and optimizes the given code by making necessory changes in code so that the improved/optimizes code can pass any rigorous tetstcases. Improved/Optimized code should pass testcases such as TLE(time limit excced) cases , memory limit excced , the worse caes or the logical tast case that is not handeled in a code but may occur in solving problem that is tackeled by code , and many more`});
+            message.push({"role":"user", "content":`Improve/Optimize the given ${changeFrom} code (Note: return only improved code as an output not any other explanation or text) . Code: ${code}`});
+            break;
+
+        default:
+            return "NULL";
+            break;
+    }
+
+    let prompt = {
+        "model": "gpt-3.5-turbo",
+        "messages": message,
+        "temperature": 1.0,
+        "top_p": 1.0,
+        "n": 1,
+        "stream": false,
+        "presence_penalty": 0,
+        "frequency_penalty": 0
+    };
+
+    // Fetching results from OpenAI API Endpoint using url
+    let res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify(prompt)
+    });
+    console.log('Made request to openai')
+
+    // Throw error on failed fetching
+    if (!res.ok) {
+        throw new Error(`Error occured at response not ok: ${res.statusText}`);
+    }
+
+    // Conveting response to json object
+    res = await res.json();
+
+    // Returning responce for given query
+    return res['choices'][0]['message']['content'];
 }
 
-
-
-
-translate.addEventListener('click',()=>{
-    const inp = input.value;
+translateLanguage.addEventListener('click',async ()=>{
+    const code = input.value;
     //selceted indexs
     const ci1 = inLangSelect.selectedIndex;
     const co1 = outLangSelect.selectedIndex;
@@ -42,10 +113,91 @@ translate.addEventListener('click',()=>{
     const co2 = inLangSelect.options[co1];
 
     // Get the value of the selected option
-    let invalue = ci2.value;
-    let outvalue = co2.value;
-    const ans = trans(inp,invalue,outvalue);
+    let changeFrom = ci2.value;
+    let changeTo = co2.value;
+    let toDo="translateLanguage";
+
+    result.innerText = "Translating Language....";
+    const ans = await getResult(code,toDo,changeTo,changeFrom);
     result.innerText = ans;
-})
+});
 
+findBugs.addEventListener('click',async ()=>{
+    const code = input.value;
+    //selceted indexs
+    const ci1 = inLangSelect.selectedIndex;
+    const co1 = outLangSelect.selectedIndex;
 
+    // Get the selected option element
+    const ci2 = inLangSelect.options[ci1];
+    const co2 = inLangSelect.options[co1];
+
+    // Get the value of the selected option
+    let changeFrom = ci2.value;
+    let changeTo = co2.value;
+    let toDo="findBugs";
+
+    result.innerText = "Finding bugs....";
+    const ans = await getResult(code,toDo,changeTo,changeFrom);
+    result.innerText = ans;
+});
+
+resolveBugs.addEventListener('click',async ()=>{
+    const code = input.value;
+    //selceted indexs
+    const ci1 = inLangSelect.selectedIndex;
+    const co1 = outLangSelect.selectedIndex;
+
+    // Get the selected option element
+    const ci2 = inLangSelect.options[ci1];
+    const co2 = inLangSelect.options[co1];
+
+    // Get the value of the selected option
+    let changeFrom = ci2.value;
+    let changeTo = co2.value;
+    let toDo="resolveBugs";
+
+    result.innerText = "Resolving bugs....";
+    const ans = await getResult(code,toDo,changeTo,changeFrom);
+    result.innerText = ans;
+});
+
+generateTestcases.addEventListener('click',async ()=>{
+    const code = input.value;
+    //selceted indexs
+    const ci1 = inLangSelect.selectedIndex;
+    const co1 = outLangSelect.selectedIndex;
+
+    // Get the selected option element
+    const ci2 = inLangSelect.options[ci1];
+    const co2 = inLangSelect.options[co1];
+
+    // Get the value of the selected option
+    let changeFrom = ci2.value;
+    let changeTo = co2.value;
+    let toDo="generateTestCase";
+
+    result.innerText = "Generating Test Cases....";
+    const ans = await getResult(code,toDo,changeTo,changeFrom);
+    result.innerText = ans;
+});
+
+improveCode.addEventListener('click',async ()=>{
+    const code = input.value;
+    //selceted indexs
+    const ci1 = inLangSelect.selectedIndex;
+    const co1 = outLangSelect.selectedIndex;
+
+    // Get the selected option element
+    const ci2 = inLangSelect.options[ci1];
+    const co2 = inLangSelect.options[co1];
+
+    // Get the value of the selected option
+    let changeFrom = ci2.value;
+    let changeTo = co2.value;
+    let toDo="improveCode";
+
+    result.innerText = "Improving Code....";
+    const ans = await getResult(code,toDo,changeTo,changeFrom);
+    result.innerText = ans;
+});
